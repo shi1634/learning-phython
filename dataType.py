00d9fa1accab061a890edc7fd51041c7b2f517f2
席少棠 <xishaotang@xishaotangdeMacBook-Pro.local>
2025-04-10 09:33:07 -0400

    Level 4 Complete

---
+++ dataType.py
@@ -0,0 +1,12 @@
+example = "I'm a string"  # The variable called example is now holding the string value "I'm a string!"
+
+a = 3
+b = 4.0
+c = True
+d = False
+e = "Hey!"
+f = None
+
+age = 32
+name = "Insert your name here"
+instrument = "Insert your instrument here"
