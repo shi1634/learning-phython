d0cac0ae92311270a458ff841e559eb19420d038
席少棠 <xishaotang@xishaotangdeMacBook-Pro.local>
2025-04-10 09:41:05 -0400

    Level 5 Complete

--- fileName.py
+++ fileName.py
@@ -1,1 +1,13 @@
-x = None
+'''
+3 = 6
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-Z, 0-9, and _)
+Variable names are case-sensitive (age, Age, and AGE are three different variables)
+A variable name cannot be any of the Python keywords.
+
+$$$Variable = "bad"
+'''
+
+
+
