1347a407fec2c2ea999dec20341c50bed31805d6
席少棠 <xishaotang@xishaotangdeMacBook-Pro.local>
2025-04-13 17:15:05 -0400

    Level 7 Complete

--- lv6.py
+++ lv6.py
@@ -1,4 +1,23 @@
-x = type(3)
-y = type("Hello")
-z = type(True)
+# variables
+my_integer = 42                
+my_float = 3.14                
+my_string = "Hello, World!"    
+my_boolean_true = True         
+my_boolean_false = False       
+my_none_value = None           
+my_negative_integer = -7       
+my_large_float = 1.23456789010
+my_single_character = 'A'      
+my_empty_string = ""           
 
+# types
+print(type(my_integer))
+print(type(my_float))
+print(type(my_string))
+print(type(my_boolean_true))
+print(type(my_boolean_false))
+print(type(my_none_value))
+print(type(my_negative_integer))
+print(type(my_large_float))
+print(type(my_single_character))
+print(type(my_empty_string))
