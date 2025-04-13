524e14d58a633dc65a4819a99568d48fbe592339
席少棠 <xishaotang@xishaotangdeMacBook-Pro.local>
2025-04-13 17:29:40 -0400

    Level 9 complete

---
+++ lv9_p1.py
@@ -0,0 +1,7 @@
+ # name and age
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+# Print 
+print("Name:", name, "| Type:", type(name))
+print("Age:", age, "| Type:", type(age))
